cd4aff2ac27adf9cdaf5dcae4bc54f607317e048
Elm <emmal.muniz@gmail.com>
2019-03-07 14:33:36 -0600

    adding game, still needs css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center"> Emma Muniz 2019 </div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import cat from './images/cat.jpeg';
+import cranky from './images/cranky.jpeg';
+import cutie from './images/cutie.jpeg';
+import grumpy from './images/grumpy.jpeg';
+import lion from './images/lion.jpg';
+import siamese from './images/siamese.jpeg';
+import tux from './images/tux.jpeg';
+import twoface from './images/twoface.jpeg';
+import white from './images/white.jpeg';
+import yelling from './images/yelling.jpeg';
+import grey from './images/grey.jpeg';
+import fluff from './images/fluff.jpeg';
+
+const images = [
+  cat,
+  cranky,
+  cutie,
+  grumpy,
+  lion,
+  siamese,
+  tux,
+  twoface,
+  white,
+  yelling,
+  grey,
+  fluff
+];
+
+export default images;
