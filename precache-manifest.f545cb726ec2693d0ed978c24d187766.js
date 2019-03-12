a22082803d9634505dea290c926dee2663c17549
Elm <emmal.muniz@gmail.com>
2019-03-12 14:19:58 -0500

    Updates

---
+++ precache-manifest.f545cb726ec2693d0ed978c24d187766.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7e77e0c411f2c93af1f46de18213de",
+    "url": "/clicky_game/static/media/tux.df7e77e0.jpeg"
+  },
+  {
+    "revision": "ff308eb30e9602494e5fed349e0180eb",
+    "url": "/clicky_game/static/media/scratchcat.ff308eb3.jpg"
+  },
+  {
+    "revision": "9e9b362676fd00f09dd1fb82bda61072",
+    "url": "/clicky_game/static/media/lion.9e9b3626.jpg"
+  },
+  {
+    "revision": "f8cf428aeba88790c600",
+    "url": "/clicky_game/static/js/runtime~main.f8cf428a.js"
+  },
+  {
+    "revision": "4c93f26f765981726a89",
+    "url": "/clicky_game/static/js/main.4c93f26f.chunk.js"
+  },
+  {
+    "revision": "315353867d2c5bffe1f5",
+    "url": "/clicky_game/static/js/2.31535386.chunk.js"
+  },
+  {
+    "revision": "4c93f26f765981726a89",
+    "url": "/clicky_game/static/css/main.7ae18ad2.chunk.css"
+  },
+  {
+    "revision": "b13c92e3f727b2b908db090306c2705c",
+    "url": "/clicky_game/index.html"
+  }
+];
